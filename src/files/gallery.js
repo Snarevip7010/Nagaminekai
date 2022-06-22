54454a75002bd0a6ac4b801b944543a137231079
Snarevip7010 <liveforever7010@gmail.com>
2022-06-22 10:45:28 +0900

    かいちくん更新

---
+++ src/files/gallery.js
@@ -0,0 +1,8 @@
+//ロード時、画面サイズがスマホサイズ(559px以下)になった時にギャラリー画像にtarget=_blankを付与
+window.onload = $(function(){
+        var windowWidth = $(window).outerWidth();
+        var maxwidth = 559;
+        if(windowWidth > maxwidth) {
+            $(".activeblank").attr('target', '_blank');
+        }
+});
